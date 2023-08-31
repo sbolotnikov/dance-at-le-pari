c16263da54fbf840d5dec929d42e7af81431acbd
Sergey Bolotnikov <sbolotnikov@gmail.com>
2023-08-31 05:19:30 -0400

    set email signin

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -5,22 +5,19 @@ export const sendEmail=async(mailData) =>{
         try {
     
         const transporter = nodemailer.createTransport(
+{
+  service:'gmail',
+  auth:{
+    user:process.env.EMAIL_SERVER_USER,
+    pass:process.env.EMAIL_PASS
+  }
+  
+}
 
-        {
-          host: process.env.EMAIL_SERVER_HOST,
-          // port: process.env.EMAIL_SERVER_PORT,
-          port: 587,
-          secure: false,
-
-          // secure: true,
-          auth: {
-            user: process.env.EMAIL_SERVER_USER, 
-            pass: process.env.EMAIL_SERVER_PASSWORD, 
-          },
-        }
         );
 
-    const result = await transporter.sendMail(mailData);
+    const result = await transporter.sendMail(mailData, function(err, info){console.log(err, info)});
+    console.log(result)
     return result;
 } catch (error) {
   return error;
