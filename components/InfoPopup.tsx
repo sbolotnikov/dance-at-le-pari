a13e05991b52fc0ccca6ce3fd14622245fd062c4
Sergey Bolotnikov <sbolotnikov@gmail.com>
2024-04-11 14:58:40 -0400

    new boarder

--- components/InfoPopup.tsx
+++ components/InfoPopup.tsx
@@ -59,16 +59,17 @@ export default function InfoPopup({ styling, onReturn }: Props) {
               rotateX: ['0deg', '0deg', '89deg', '89deg', '90deg'],
               x: ['0vw', '0vw', '0vw', '0vw', '-100vw'],
             }}
-            className="m-auto  max-w-[600px] max-h-[600px]  border-2 border-brightAccent overflow-hidden  rounded-lg w-[97%] h-full flex flex-col justify-center items-center relative bg-lightMainColor dark:bg-darkMainColor dark:text-lightMainColor text-darkMainColor"
+            className="m-auto  max-w-[600px] max-h-[600px]  border border-brightAccent overflow-hidden  rounded-lg w-[97%] h-full flex flex-col justify-center items-center relative bg-darkMainColor text-lightMainColor"
           >
             <div className="absolute w-3/4 h-3/4 flex flex-col justify-around items-center xs:text-sm xs:leading-4">
+            <label className="px-1 py-2 border-2 border-solid border-transparent rounded-sm w-[80%] m-1 text-center font-[GoudyBookletter] text-lg  ">
+                {styling.heading}
+              </label>
               <div
                 className="px-1 py-2 border-2 border-solid border-transparent rounded-sm w-full m-1 text-center flex flex-col justify-center items-center overflow-auto"
                 dangerouslySetInnerHTML={{ __html: styling.text }}
               />
-              <label className="px-1 py-2 border-2 border-solid border-transparent rounded-sm w-[80%] m-1 text-center font-[GoudyBookletter]   ">
-                {styling.heading}
-              </label>
+              
               <div className="w-full flex flex-row justify-evenly">
                 <button
                   className="px-1 py-2 border-2 border-solid border-transparent rounded-sm w-[48%] m-1 text-center font-[GoudyBookletter] "
