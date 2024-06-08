6a5f7f0c6ad7f0c63acae80688564b6900f0fedf
Sergey Bolotnikov <sbolotnikov@gmail.com>
2024-06-08 00:29:07 -0400

    change AuthOpttions to fix deploy 2

--- app/api/purchases/route.ts
+++ app/api/purchases/route.ts
@@ -2,7 +2,8 @@
 import { NextResponse } from 'next/server'
 import { prisma } from '@/lib/prisma';
 import { getServerSession } from 'next-auth/next'
-import { authOptions } from '../auth/[...nextauth]/route'
+import { authOptions } from '@/lib/configs/authOptions';
+ 
 
 export async function GET() {
     const session = await getServerSession(authOptions)
--- app/api/schedule/route.ts
+++ app/api/schedule/route.ts
@@ -1,7 +1,8 @@
 import { getServerSession } from 'next-auth/next'
 import { NextResponse } from 'next/server'
-import { authOptions } from '../auth/[...nextauth]/route'
+ 
 import { prisma } from '@/lib/prisma';
+import { authOptions } from '@/lib/configs/authOptions';
 
 export async function GET(request: Request) {
   const session = await getServerSession(authOptions)
