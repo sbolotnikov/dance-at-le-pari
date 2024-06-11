aa1b080cb844234e1c78aaeacce2a13a56099ad4
Sergey Bolotnikov <sbolotnikov@gmail.com>
2024-06-11 05:45:57 -0400

    blog, calendar, activities, wedding, main page added share capabilities 3  try

--- app/calendar/layout.tsx
+++ app/calendar/layout.tsx
@@ -1,19 +1,15 @@
-import { Metadata, ResolvingMetadata } from 'next';
-export const generateMetadata = async (
-  
-  parent: ResolvingMetadata
-): Promise<Metadata> => {
+import { Metadata } from 'next';
+export const metadata: Metadata = {
   // read route params
  
  
   // optionally access and extend (rather than replace) parent metadata
 //   const previousImages = (await parent).openGraph?.images || [];
  
-   return {  
     openGraph: { title: "Page: Calendar | Dance at Le Pari Studio", url: `${process.env.NEXTAUTH_URL}/calendar` },
     description: "Dance Events in New Jersey! Best Dance Studio in NJ with many dance options.  You want to have fun, go out dancing or find smth different to do, come to us! We offer dance events running every week as well as many dance classes to join! Dance Events and Dance Classes Calendar."  ,
     title: "Page: Calendar | Dance at Le Pari Studio" 
-  };
+
 };
 
 export default function PageLayout({
--- app/dancing/layout.tsx
+++ app/dancing/layout.tsx
@@ -1,19 +1,13 @@
-import { Metadata, ResolvingMetadata } from 'next';
-export const generateMetadata = async (
-  
-  parent: ResolvingMetadata
-): Promise<Metadata> => {
-  // read route params
+import { Metadata } from 'next';
+export const metadata: Metadata = {
  
  
   // optionally access and extend (rather than replace) parent metadata
 //   const previousImages = (await parent).openGraph?.images || [];
- 
-   return {  
+   
     openGraph: { title: "Page: Activities | Dance at Le Pari Studio", url: `${process.env.NEXTAUTH_URL}/dancing` },
     description: "All studio activities. Can pay for all services online as contactless method of payment: online &amp; in studio groups &amp; privates, dance social party admission, dance host cost, online &amp; in studio wedding dance lessons, floor fee. You can find Privacy Policy of Le Pari Dance Center. " ,
     title: "Page: Activities | Dance at Le Pari Studio" 
-  };
 };
 
 export default function PageLayout({
