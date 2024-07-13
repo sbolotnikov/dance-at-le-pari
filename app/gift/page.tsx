ba8f0de4e5c031d6bb539d998ca1ae3cc8875b4e
Sergey Bolotnikov <sbolotnikov@gmail.com>
2024-07-13 10:13:44 -0400

    Gift certificate page. layout fix

--- app/gift/page.tsx
+++ app/gift/page.tsx
@@ -45,8 +45,7 @@ const page = (props: Props) => {
       if (revealCloud == true) {
         setRevealCloud(false);
         setImg(fileLink);
-      }
-      else {
+      } else {
         setRevealCloud(false);
         console.log('file link', fileLink);
         setImg(fileLink);
@@ -56,9 +55,9 @@ const page = (props: Props) => {
   return (
     <PageWrapper className="absolute top-0 left-0 w-full h-screen flex items-center justify-center">
       {revealCloud && <ChoosePicture onReturn={onReturnPicture} />}
-      <div className="blurFilter border-0 rounded-md p-2 shadow-2xl w-[90%] max-w-[850px] max-h-[85%] overflow-y-auto md:w-full md:mt-8 bg-lightMainBG/70 dark:bg-darkMainBG/70">
-        <div className="w-full h-full flex flex-col justify-center items-center border rounded-md border-lightMainColor dark:border-darkMainColor relative p-2">
-          <div className="container mx-auto p-4">
+      <div className="blurFilter border-0 rounded-md p-2 shadow-2xl w-[90%] max-w-[850px] h-full max-h-[85%]  md:w-full md:mt-8 bg-lightMainBG/70 dark:bg-darkMainBG/70">
+        <div className="w-full h-full flex flex-col justify-center items-center border rounded-md border-lightMainColor dark:border-darkMainColor relative p-2 overflow-y-auto">
+          <div className=" absolute top-0 left-0 w-full h-fit p-2">
             <h3 className="w-full uppercase xs:text-md sm:text-xl phone:text-2xl tablet:text-3xl text-center">
               Gift Certificates
             </h3>
@@ -83,30 +82,38 @@ const page = (props: Props) => {
                     setChoosenOption(option);
                   }}
                 />
-                {priceOptions!==null &&<button
-                  className=" btnFancy"
-                  onClick={() => {
-                    const p1 = {
-                      id: choosenOption,
-                      image: img,
-                      eventtype: 'Gift Certificate',
-                      price: priceOptions
-                        ? priceOptions[choosenOption].price
-                        : 0,
-                      tag: priceOptions ? priceOptions[choosenOption].tag : '',
-                      amount: 1,
-                      seat: null,
-                      table: null,
-                      date: null,
-                    };
-                    dispatch(addItem(p1));
-                  }}
-                >
-                  Add To Cart
-                </button>}
+                {priceOptions !== null && (
+                  <button
+                    className=" btnFancy"
+                    onClick={() => {
+                      const p1 = {
+                        id: choosenOption,
+                        image: img,
+                        eventtype: 'Gift Certificate',
+                        price: priceOptions
+                          ? priceOptions[choosenOption].price
+                          : 0,
+                        tag: priceOptions
+                          ? priceOptions[choosenOption].tag
+                          : '',
+                        amount: 1,
+                        seat: null,
+                        table: null,
+                        date: null,
+                      };
+                      dispatch(addItem(p1));
+                    }}
+                  >
+                    Add To Cart
+                  </button>
+                )}
                 {session && session.user.role == 'Admin' && (
                   <div className="w-24">
-                    <div onClick={() => {setRevealCloud(true)}}>
+                    <div
+                      onClick={() => {
+                        setRevealCloud(true);
+                      }}
+                    >
                       {img !== null && img !== '' && img !== undefined ? (
                         <ImgFromDb
                           url={img}
@@ -123,7 +130,6 @@ const page = (props: Props) => {
                 )}
                 {session && session.user.role == 'Admin' && (
                   <div className="flex flex-col justify-between items-center w-full">
-                     
                     <PriceOptions
                       options={priceOptions}
                       onChange={(options: TPriceOption[] | null) => {
@@ -149,7 +155,10 @@ const page = (props: Props) => {
                             'Content-Type': 'application/json',
                           },
                           body: JSON.stringify({
-                            giftCertificates: JSON.stringify({priceOptions,img}),
+                            giftCertificates: JSON.stringify({
+                              priceOptions,
+                              img,
+                            }),
                           }),
                         })
                           .then((res) => res)
