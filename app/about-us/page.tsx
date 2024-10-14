9d4db6640bce81db6c9ca7e2b3390c2b6ceb0944
Sergey Bolotnikov <sbolotnikov@gmail.com>
2024-10-14 13:35:18 -0400

    few redirects from old website

---
+++ app/about-us/page.tsx
@@ -0,0 +1,18 @@
+"use client"
+import { useRouter } from "next/navigation";
+import { useEffect } from "react"
+
+
+type Props = {}
+
+const page = (props: Props) => {
+   const router = useRouter();
+   useEffect(() => {
+      router.push("/about_us/welcome");
+   }, [])  
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
