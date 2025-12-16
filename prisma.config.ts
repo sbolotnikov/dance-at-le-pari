634ad94319bc3943ca0ed126c9d3b7eedf8e3019
Sergey Bolotnikov <sbolotnikov@gmail.com>
2025-12-16 13:24:11 -0500

    fix prisma 1

---
+++ prisma.config.ts
@@ -0,0 +1,25 @@
+import 'dotenv/config'
+
+
+
+
+function env(key: string): string {
+  const value = process.env[key];
+  if (!value) {
+    throw new Error(`Missing environment variable: ${key}`);
+  }
+  return value;
+}
+
+const config = {
+  schema: 'prisma/schema.prisma',
+  migrations: { 
+    path: 'prisma/migrations',
+    seed: 'tsx prisma/seed.ts',
+  },
+  datasource: { 
+    url: env("DATABASE_URL") 
+  }
+};
+
+export default config;
